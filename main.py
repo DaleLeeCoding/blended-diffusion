64ab7b8afeb65aabf96887b46f4ed408afc70640
SEUNGWOO LEE <seungwoo.lee@snu.ac.kr>
2022-07-04 10:49:01 +0900

    Add flag to resume training

--- main.py
+++ main.py
@@ -6,9 +6,15 @@ import pandas as pd
 if __name__ == "__main__":
     args = get_arguments()
     xlsx = pd.read_excel('./DrawBench Prompts.xlsx')
+    idx = 0
+    flag = 114
     for pr, cat in zip(xlsx['Prompts'], xlsx['Category']):
-        args.prompt = pr
-        args.category = cat
-        image_editor = ImageEditor(args)
-        image_editor.edit_image_by_prompt()
-    # image_editor.reconstruct_image()
+        idx += 1
+        if idx > flag:
+            args.prompt = pr
+            args.category = cat
+            print(f"Prompt: {args.prompt}")
+            image_editor = ImageEditor(args)
+            image_editor.edit_image_by_prompt()
+            # image_editor.reconstruct_image()
+
